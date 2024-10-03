d9fe590b5e15dbc6e0e12cdd366a3e9d6231eca5
Enzo Vallet <evallet11@myges.fr>
2024-10-03 14:31:50 +0200

    add: arrow face good direction

--- godot/project-roguelike/player.gd
+++ godot/project-roguelike/player.gd
@@ -30,10 +30,17 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 	
 	# Animation
+	var mousePos = get_global_mouse_position().normalized()
 	if direction.x < 0:
 		flipH = true
 	elif direction.x > 0:
 		flipH = false
+	if is_shooting:
+		# Turn animation if aim behind the player
+		if position.normalized().x - mousePos.normalized().x > 0:
+			flipH = true
+		else:
+			flipH = false
 	sprite.set_flip_h(flipH)
 	if direction != Vector2.ZERO:
 		anim_name = "walk"
@@ -53,6 +60,7 @@ func _input(_event: InputEvent) -> void:
 		is_shooting = true
 		await get_tree().create_timer(0.5).timeout
 		shoot_arrow()
+		
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
@@ -60,6 +68,7 @@ func _on_animated_sprite_2d_animation_finished() -> void:
 		is_shooting = false
 
 func shoot_arrow() -> void:
+	# Instanciate one arrow
 	var arrow_scene = arrow.instantiate()
 	arrow_scene.position = self.position
 	arrow_scene.direction_arrow = (position - get_global_mouse_position()).normalized()
