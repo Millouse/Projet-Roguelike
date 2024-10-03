9b6315dc2a956aea314e31b76d37ac66d6b890fd
Enzo Vallet <evallet11@myges.fr>
2024-10-03 12:04:05 +0200

    add: player shoot arrow

---
+++ godot/project-roguelike/arrow.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var speed = 700
+
+var direction_arrow: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position -= direction_arrow * speed * delta
+	pass
--- godot/project-roguelike/player.gd
+++ godot/project-roguelike/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+# Scene
+var arrow = preload("res://arrow.tscn")
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -14,6 +17,9 @@ var is_shooting = false
 var flipH = false
 var anim_name: String = "idle"
 
+#func _ready() -> void:
+	#animation_finished", Callable(self, "_on_animated_sprite_2d_animation_finished"))
+
 func _physics_process(_delta: float) -> void:
 	# Movement
 	var vy = Input.get_axis("move_up", "move_down")
@@ -24,6 +30,11 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 	
 	# Animation
+	if direction.x < 0:
+		flipH = true
+	elif direction.x > 0:
+		flipH = false
+	sprite.set_flip_h(flipH)
 	if direction != Vector2.ZERO:
 		anim_name = "walk"
 	elif is_shooting:
@@ -31,22 +42,27 @@ func _physics_process(_delta: float) -> void:
 	else:
 		anim_name = "idle"
 	sprite.play(anim_name)
-	if direction.x < 0:
-		flipH = true
-	else:
-		flipH = false
-	sprite.set_flip_h(flipH)
 
 
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("restart_scene"):
 		get_tree().reload_current_scene()
 
 	if Input.is_action_just_pressed("fire"):
+		print("je tire mgl")
 		is_shooting = true
+		await get_tree().create_timer(0.5).timeout
+		shoot_arrow()
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if anim_name == "shoot_arrow":
 		is_shooting = false
-	pass # Replace with function body.
+
+func shoot_arrow() -> void:
+	var arrow_scene = arrow.instantiate()
+	arrow_scene.position = self.position
+	arrow_scene.direction_arrow = (position - get_global_mouse_position()).normalized()
+	# Rotate toward mouse
+	arrow_scene.look_at(get_global_mouse_position())
+	get_parent().add_child(arrow_scene)
